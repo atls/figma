001d0129ba274124e0a953722a50ae5cd80c9f6b
Kirill Zolottsev <carmine@atls.team>
2022-09-14 20:46:09 +0400

    feat: fontSizes is divided into groups

--- theme/theme-font-sizes-generator/src/Constants.ts
+++ theme/theme-font-sizes-generator/src/Constants.ts
@@ -1,33 +1,52 @@
 // eslint-disable-next-line no-shadow
+export enum Group {
+  SMALL = 'small',
+  NORMAL = 'normal',
+  MEDIUM = 'medium',
+  LARGE = 'large',
+}
+
+// eslint-disable-next-line no-shadow
 export enum FontSize {
-  Atomic = 'atomic',
-  Micro = 'micro',
+  SemiTiny = 'semiTiny',
   Tiny = 'tiny',
+  SemiSmall = 'semiSmall',
   Small = 'small',
+  SemiReduced = 'semiReduced',
   Reduced = 'reduced',
+  SemiDefault = 'semiDefault',
 
-  Normal = 'normal',
-  Regular = 'regular',
-  Extra = 'extra',
+  Default = 'default',
 
-  Medium = 'medium',
+  SemiIncreased = 'semiIncreased',
   Increased = 'increased',
-
+  SemiLarge = 'semiLarge',
   Large = 'large',
+  SemiHuge = 'semiHuge',
   Huge = 'huge',
+  SemiGiant = 'semiGiant',
   Giant = 'giant',
 }
 
-export const FontSmallSizes = [
-  FontSize.Atomic,
-  FontSize.Micro,
+export const FontSizeNames = [FontSize.Default]
+
+export const groupNamesLessThanDefault = [
+  FontSize.SemiTiny,
   FontSize.Tiny,
+  FontSize.SemiSmall,
   FontSize.Small,
+  FontSize.SemiReduced,
   FontSize.Reduced,
+  FontSize.SemiDefault,
 ]
 
-export const FontNormalSizes = [FontSize.Normal, FontSize.Regular, FontSize.Extra]
-
-export const FontMediumSizes = [FontSize.Medium, FontSize.Increased]
-
-export const FontLargeSizes = [FontSize.Large, FontSize.Huge, FontSize.Giant]
+export const groupNamesGreaterThanDefault = [
+  FontSize.SemiIncreased,
+  FontSize.Increased,
+  FontSize.SemiLarge,
+  FontSize.Large,
+  FontSize.SemiHuge,
+  FontSize.Huge,
+  FontSize.SemiGiant,
+  FontSize.Giant,
+]
--- theme/theme-font-sizes-generator/src/strategy/SimpleMappingStrategy.ts
+++ theme/theme-font-sizes-generator/src/strategy/SimpleMappingStrategy.ts
@@ -1,72 +1,90 @@
-import { Text }            from 'figma-js'
+import { Text }                      from 'figma-js'
 
-import { FontLargeSizes }  from '../Constants'
-import { FontMediumSizes } from '../Constants'
-import { FontNormalSizes } from '../Constants'
-import { FontSmallSizes }  from '../Constants'
-import { Strategy }        from './Strategy'
+import { Group }                     from '../Constants'
+import { Strategy }                  from './Strategy'
+import { groupNamesLessThanDefault } from '../Constants'
 
 export class SimpleMappingStrategy extends Strategy {
   fillSmallSizes(fontSizes) {
-    return fontSizes.reduce(
-      (result, fontSize, index) => ({
-        ...result,
-        [FontSmallSizes[index]]: fontSize,
-      }),
-      {}
-    )
+    const fontSizeMiddle = Math.ceil(fontSizes.length / 2)
+
+    return fontSizes.reduce((result, fontSize, index) => {
+      if (fontSize > fontSizeMiddle) {
+        return {
+          ...result,
+          [groupNamesLessThanDefault[index]]: fontSize,
+        }
+      }
+      return result
+    }, {})
   }
 
   fillNormalSizes(fontSizes) {
-    return fontSizes.reduce(
-      (result, fontSize, index) => ({
-        ...result,
-        [FontNormalSizes[index]]: fontSize,
-      }),
-      {}
-    )
+    const fontSizeMiddle = Math.ceil(fontSizes.length / 2)
+
+    return fontSizes.reduce((result, fontSize, index) => {
+      if (fontSize > fontSizeMiddle) {
+        return {
+          ...result,
+          [groupNamesLessThanDefault[index]]: fontSize,
+        }
+      }
+      return result
+    }, {})
   }
 
   fillMediumSizes(fontSizes) {
-    return fontSizes.reduce(
-      (result, fontSize, index) => ({
-        ...result,
-        [FontMediumSizes[index]]: fontSize,
-      }),
-      {}
-    )
+    const fontSizeMiddle = Math.ceil(fontSizes.length / 2)
+
+    return fontSizes.reduce((result, fontSize, index) => {
+      if (fontSize > fontSizeMiddle) {
+        return {
+          ...result,
+          [groupNamesLessThanDefault[index]]: fontSize,
+        }
+      }
+      return result
+    }, {})
   }
 
   fillLargeSizes(fontSizes) {
-    return fontSizes.reduce(
-      (result, fontSize, index) => ({
-        ...result,
-        [FontLargeSizes[index]]: fontSize,
-      }),
-      {}
-    )
+    const fontSizeMiddle = Math.ceil(fontSizes.length / 2)
+
+    return fontSizes.reduce((result, fontSize, index) => {
+      if (fontSize > fontSizeMiddle) {
+        return {
+          ...result,
+          [groupNamesLessThanDefault[index]]: fontSize,
+        }
+      }
+
+      return result
+    }, {})
   }
 
   execute(textNodes: Text[] = []) {
     const stat = this.getStat(textNodes)
 
     const fontSizes = Array.from(stat.keys()).sort((a, b) => a - b)
-    const [normal] = [...fontSizes].sort((a, b) => (stat.get(b) || 0) - (stat.get(a) || 0))
 
-    const normalIndex = fontSizes.indexOf(normal)
-    const mediumIndex = normalIndex + FontNormalSizes.length
-    const largeIndex = mediumIndex + FontMediumSizes.length
-
-    const smallSizes = fontSizes.slice(0, normalIndex)
-    const normalSizes = fontSizes.slice(normalIndex, mediumIndex)
-    const mediumSizes = fontSizes.slice(mediumIndex, largeIndex)
-    const largeSizes = fontSizes.slice(largeIndex, fontSizes.length)
+    const smallSizes = fontSizes.filter((size) => size < 14)
+    const normalSizes = fontSizes.filter((size) => size < 24 && size > 14)
+    const mediumSizes = fontSizes.filter((size) => size < 50 && size > 24)
+    const largeSizes = fontSizes.filter((size) => size > 50)
 
     return {
-      ...this.fillSmallSizes(smallSizes),
-      ...this.fillNormalSizes(normalSizes),
-      ...this.fillMediumSizes(mediumSizes),
-      ...this.fillLargeSizes(largeSizes),
+      [Group.SMALL]: {
+        ...this.fillSmallSizes(smallSizes),
+      },
+      [Group.NORMAL]: {
+        ...this.fillNormalSizes(normalSizes),
+      },
+      [Group.MEDIUM]: {
+        ...this.fillMediumSizes(mediumSizes),
+      },
+      [Group.LARGE]: {
+        ...this.fillLargeSizes(largeSizes),
+      },
     }
   }
 }
