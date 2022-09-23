72de3f5099dc0e4a815de7654fd8a8a9a94e6433
Kirill Zolottsev <carmine@atls.team>
2022-09-23 18:16:41 +0400

    feat: lineHeights generation, tests

--- theme/theme-line-heights-generator/src/strategy/SimpleMappingStrategy.ts
+++ theme/theme-line-heights-generator/src/strategy/SimpleMappingStrategy.ts
@@ -1,13 +1,36 @@
-import { Text }     from 'figma-js'
+import { Text }                  from 'figma-js'
 
-import { Strategy } from './Strategy'
+import { Strategy }              from './Strategy'
+import { lineHeightSmallSizes }  from '../Constants'
+import { lineHeightMediumSizes } from '../Constants'
+import { lineHeightLargeSizes }  from '../Constants'
 
 export class SimpleMappingStrategy extends Strategy {
-  fillLineHeights(lineHeights) {
+  fillSmallLineHeights(lineHeights) {
     return lineHeights.reduce(
       (result, lineHeight, index) => ({
         ...result,
-        [index]: Number(lineHeight),
+        [lineHeightSmallSizes[index]]: Number(lineHeight),
+      }),
+      {}
+    )
+  }
+
+  fillMediumLineHeights(lineHeights) {
+    return lineHeights.reduce(
+      (result, lineHeight, index) => ({
+        ...result,
+        [lineHeightMediumSizes[index]]: Number(lineHeight),
+      }),
+      {}
+    )
+  }
+
+  fillLargeLineHeights(lineHeights) {
+    return lineHeights.reduce(
+      (result, lineHeight, index) => ({
+        ...result,
+        [lineHeightLargeSizes[index]]: Number(lineHeight),
       }),
       {}
     )
@@ -17,13 +40,22 @@ export class SimpleMappingStrategy extends Strategy {
     const stat = this.getStat(textNodes)
 
     const lineHeights = Array.from(stat.keys()).sort((a, b) => {
-      const x: any = a.split('.')
-      const y: any = b.split('.')
-      return x[0] - y[0] || x[1] - y[1]
+      const x = a.split('.')
+      const y = b.split('.')
+
+      return Number(x[0]) - Number(y[0]) || Number(x[1]) - Number(y[1])
     })
 
+    const smallLineHeights = lineHeights.filter((lineHeight) => Number(lineHeight) < 1)
+    const mediumLineHeights = lineHeights.filter(
+      (lineHeight) => Number(lineHeight) >= 1 && Number(lineHeight) < 2
+    )
+    const largeLineHeights = lineHeights.filter((lineHeight) => Number(lineHeight) >= 2)
+
     return {
-      ...this.fillLineHeights(lineHeights),
+      ...this.fillSmallLineHeights(smallLineHeights),
+      ...this.fillMediumLineHeights(mediumLineHeights),
+      ...this.fillLargeLineHeights(largeLineHeights),
     }
   }
 }
