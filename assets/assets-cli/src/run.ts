3becc5fb949292537284a1e0a0b1c86d182c36c4
Nikita Elfimov <nelfimov@mail.ru>
2023-10-26 21:23:40 +0300

    chore: code changes for publish

--- assets/assets-cli/src/run.ts
+++ assets/assets-cli/src/run.ts
@@ -7,7 +7,6 @@ export const run = async (fileId, documentId, output) => {
   const loader = new FigmaFileLoader()
 
   const node: Node = await loader.loadDocument(fileId, documentId)
-
   const assets = new FigmaAssets(fileId, node, output)
   assets.generate()
 }
