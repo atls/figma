fb2d43da26713a09446d71b36fe9e35d9b0cd27c
Dmitry <dimafomin02@gmail.com>
2024-12-04 11:08:15 +0300

    feat: add process-file theme path

--- fragments/fragments-cli/src/run.ts
+++ fragments/fragments-cli/src/run.ts
@@ -1,19 +1,49 @@
+import { transform }                    from '@babel/standalone'
+
 import path                             from 'path'
 import prettier                         from 'prettier'
 import { promises as fs }               from 'fs'
+import { readFileSync }                 from 'fs'
+import { join }                         from 'path'
 
 import { FigmaFileLoader }              from '@atls/figma-file-loader'
 import { FigmaThemeFragmentsGenerator } from '@atls/figma-fragments-generator'
 
-export const run = async (fileId, nodeId, output) => {
+const processFile = (filePath: string): any => {
+  const replacementsFile = readFileSync(filePath.replace('.js', '.ts')).toString('utf-8')
+  const { code } = transform(replacementsFile, {
+    presets: ['env'],
+    plugins: ['transform-modules-commonjs'],
+  })
+
+  if (!code) throw Error('Could not read the file')
+
+  // eslint-disable-next-line no-eval, security/detect-eval-with-expression
+  const module = { exports: {} }
+  const exports = module.exports
+  const require = (modulePath) => {
+    const absolutePath = path.resolve(path.dirname(filePath), modulePath)
+    return processFile(absolutePath)
+  }
+  eval(`
+    (function(exports, module, require) {
+      ${code}
+    })(exports, module, require);
+  `)
+  return module.exports
+}
+
+export const run = async (fileId, nodeId, output, themeFilePath) => {
+  const theme: Record<string, Record<string, string>> = processFile(
+    join(process.cwd(), themeFilePath)
+  ).lightThemeTokens
+
   const loader = new FigmaFileLoader()
   const generator = new FigmaThemeFragmentsGenerator()
 
   const response = await loader.loadNode(fileId, nodeId)
 
-  console.log(response.nodes[nodeId]?.document)
-
-  const component = generator.generate(response)
+  const component = generator.generate(response, theme)
 
   const target = path.join(output, 'fragments.tsx')
 
