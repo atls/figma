41d73714b40ab71d257a3ac015c31dafdc49872f
Dmitry <dimafomin02@gmail.com>
2024-12-16 10:04:04 +0300

    feat: add is-instance to figma utils

--- utils/utils/src/walk.ts
+++ utils/utils/src/walk.ts
@@ -1,12 +1,15 @@
-import { Node }  from 'figma-js'
-import { Text }  from 'figma-js'
-import { Frame } from 'figma-js'
+import { Node }     from 'figma-js'
+import { Text }     from 'figma-js'
+import { Frame }    from 'figma-js'
+import { Instance } from 'figma-js'
 
 const isEmpty = (node: any) => !node || Object.keys(node).length === 0
 
 export const isText = (node: Node): node is Text => node.type === 'TEXT'
 
 export const isFrame = (node: Node): node is Frame => node.type === 'FRAME'
+
+export const isInstance = (node: Node): node is Instance => node.type === 'INSTANCE'
 
 export const walk = (targetNode: any, cb: (node: any) => any) => {
   if (isEmpty(targetNode) || typeof targetNode === 'string' || typeof targetNode === 'number') {
