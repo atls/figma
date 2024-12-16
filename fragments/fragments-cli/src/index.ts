50a92274357d0026c90b2a3e5941101f9cb83c8c
Dmitry <dimafomin02@gmail.com>
2024-12-16 10:07:19 +0300

    feat: add question to get figma access token to figma-cli

--- fragments/fragments-cli/src/index.ts
+++ fragments/fragments-cli/src/index.ts
@@ -25,22 +25,20 @@ if (options.verbose) {
 if (!fileId) {
   logger.error('fileId', 'Figma file id required.')
 } else {
-  // const readline = createInterface({
-  //   input: process.stdin,
-  //   output: process.stdout,
-  // })
+  const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
 
-  // readline.question(`Enter your Figma access token:\n`, (id) => {
-  //   if (!id || id === '') throw Error('ID must not be empty')
-  //   // eslint-disable-next-line dot-notation
+  readline.question(`Enter your Figma access token:\n`, (id) => {
+    if (!id || id === '') throw Error('ID must not be empty')
+    // eslint-disable-next-line dot-notation
+    process.env['FIGMA_TOKEN'] = id
 
-  //   readline.close()
+    readline.close()
 
-  // })
-
-  process.env['FIGMA_TOKEN'] = 'secret'
-
-  run(fileId, options.nodeId, options.output, options.theme)
-    .then(() => logger.info('info', 'Fragments successful generated'))
-    .catch((error) => logger.error('error', error.message))
+    run(fileId, options.nodeId, options.output, options.theme)
+      .then(() => logger.info('info', 'Fragments successful generated'))
+      .catch((error) => logger.error('error', error))
+  })
 }
