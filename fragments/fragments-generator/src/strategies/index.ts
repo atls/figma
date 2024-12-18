3446fffa49039f913c261aee396b6eb987b64895
Dmitry Fomin <80476878+Amiditin@users.noreply.github.com>
2024-12-18 14:22:36 +0300

    chore: bump figma fragments version (#47)

--- fragments/fragments-generator/src/strategies/index.ts
+++ fragments/fragments-generator/src/strategies/index.ts
@@ -1,5 +1,5 @@
-export * from './create-box/index.js'
 export * from './create-button/index.js'
+export * from './create-box/index.js'
 export * from './create-fragment/index.js'
 export * from './create-input/index.js'
 export * from './create-text/index.js'
