4d94eb2ef3a3e71ba6b9c004aaadb7e8b70cfe1f
Dmitry <dimafomin02@gmail.com>
2025-01-16 02:45:32 +0300

    feat(figma): add file-image-fills to file loader

--- loaders/file-loader/src/FigmaFileLoader.ts
+++ loaders/file-loader/src/FigmaFileLoader.ts
@@ -1,10 +1,11 @@
-import type { ClientInterface }   from 'figma-js'
-import type { FileImageResponse } from 'figma-js'
-import type { FileResponse }      from 'figma-js'
-import type { FileNodesResponse } from 'figma-js'
-import type { Node }              from 'figma-js'
+import type { ClientInterface }        from 'figma-js'
+import type { FileImageResponse }      from 'figma-js'
+import type { FileResponse }           from 'figma-js'
+import type { FileNodesResponse }      from 'figma-js'
+import type { FileImageFillsResponse } from 'figma-js'
+import type { Node }                   from 'figma-js'
 
-import { Client }                 from 'figma-js'
+import { Client }                      from 'figma-js'
 
 export class FigmaFileLoader {
   figma: ClientInterface
@@ -43,5 +44,10 @@ export class FigmaFileLoader {
       scale: 1,
     })
     return data.images
+  }
+
+  async fileImageFills(fileId: string): Promise<FileImageFillsResponse['meta']['images']> {
+    const { data } = await this.figma.fileImageFills(fileId)
+    return data.meta.images
   }
 }
